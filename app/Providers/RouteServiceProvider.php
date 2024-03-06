40888d12b84b26bc0b9d379912c6c8a21e390264
Tiago Tavares <tiago@cyber-duck.co.uk>
2024-03-06 22:38:18 +0000

    Run Laravel pint to get the latest styles

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -40,7 +40,7 @@ class RouteServiceProvider extends ServiceProvider
         $this->routes(function () {
             Route::prefix('api')
                 ->middleware('api')
-                ->namespace($this->namespace . '\\Api')
+                ->namespace($this->namespace.'\\Api')
                 ->group(base_path('routes/api.php'));
 
             Route::middleware('web')
